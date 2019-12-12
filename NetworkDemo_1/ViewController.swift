20391e32f9e760d5b749014f3132fcecbaf771cc
sergei <sergei.sai@bk.ru>
2019-12-12 15:52:56 +0300

    Initial Commit

---
+++ NetworkDemo_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkDemo_1
+//
+//  Created by Admin on 12.12.2019.
+//  Copyright © 2019 sergei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
